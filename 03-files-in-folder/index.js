84183349a65a236035da146d80606775ace9a31a
neemkashu <sparjinkust@mail.ru>
2022-11-01 23:31:58 +0300

    feat: implement table with files properties via callbacks

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 
 let subfolderPath = path.join(__dirname,'secret-folder');
 
-const files = fs.readdir(subfolderPath,  { withFileTypes: true }, (err, files) => {
-  console.debug(err, files);
+fs.readdir(subfolderPath,  { withFileTypes: true }, (err, files) => {
+  files.forEach( file => {
+    let filePath = path.join(subfolderPath, file.name);
+    if ( file.isFile() ) {
+      fs.stat(filePath, (err, stats)=>{
+        file.size = stats.size;
+        console.log(file.name, ' - ', path.extname(filePath).substring(1),' - ', file.size, ' bytes');  
+      });      
+    }  
+  });
 });
