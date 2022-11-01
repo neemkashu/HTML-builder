eaea0b9a0548907b71530775d7c158ee14e33555
neemkashu <sparjinkust@mail.ru>
2022-11-01 18:16:27 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout, stdin } = process;
+let flagOfSIGINT = 0;
+
+stdout.write('Type a text and press ENTER\n');
+fs.writeFile(
+  path.resolve(__dirname, 'notes.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+process.on('exit', () => {
+  if (!flagOfSIGINT) stdout.write('The changes are saved in notes.txt');
+});
+process.on('SIGINT', () => {
+  flagOfSIGINT++;
+  stdout.write('The changes are saved in notes.txt');  
+  process.exit();
+});
+stdin.on('data', data => {
+  if (data.toString().slice(0,4) === 'exit') process.exit();
+  console.debug(data.toString().charCodeAt(data.toString().length - 1));
+  fs.appendFile(
+    path.resolve(__dirname, 'notes.txt'),
+    data.toString(),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+});
+
