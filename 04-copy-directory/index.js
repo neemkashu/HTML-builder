9ae813620d9deb79e43945045a06879e7abe8fdc
neemkashu <sparjinkust@mail.ru>
2022-11-02 16:10:56 +0300

    feat: implement creating new copy of target folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+import path from 'node:path';
+import { mkdir } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { rm } from 'node:fs/promises';
 
+async function copyDir (folder) {
+  try {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const newFolder = folder + '-copy';
+    const pathNewFolder = path.join( __dirname, newFolder);
+    
+    const isRemoved = await rm(pathNewFolder, { force: true, maxRetries: 0, recursive: true })
+    
+    if (isRemoved === undefined) {
+      mkdir(pathNewFolder, { recursive: true });
+    }
+    // for (const file of files)
+    //   if (file.isFile()) {
+    //     const statOfFile = await stat(path.join(pathToFolder,file.name));
+    //     console.log(file.name, ' - ',
+    //                 path.extname(path.join(pathToFolder,file.name)).substring(1),' - ',
+    //                 statOfFile.size, ' bytes');  
+    //   }
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+copyDir('files');
