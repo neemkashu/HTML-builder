f02f778e4feb9d68a599eb97e789c9214e3461ed
neemkashu <sparjinkust@mail.ru>
2022-11-02 13:23:57 +0300

    feat: implement promises instead of callbacks in the task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const fs = require('fs');
 
-let subfolderPath = path.join(__dirname,'secret-folder');
-
-fs.readdir(subfolderPath,  { withFileTypes: true }, (err, files) => {
-  files.forEach( file => {
-    let filePath = path.join(subfolderPath, file.name);
-    if ( file.isFile() ) {
-      fs.stat(filePath, (err, stats)=>{
-        file.size = stats.size;
-        console.log(file.name, ' - ', path.extname(filePath).substring(1),' - ', file.size, ' bytes');  
-      });      
-    }  
-  });
-});
+//let subfolderPath = path.join(__dirname,'secret-folder'); // this is also need to be promise
+async function readStats(folder) {
+  try {
+    const pathToFolder = path.join(__dirname,folder);
+    const files = await fs.promises.readdir(pathToFolder, { withFileTypes: true });
+    for (const file of files)
+      if (file.isFile()) {
+        const statOfFile = await fs.promises.stat(path.join(pathToFolder,file.name));
+        console.log(file.name, ' - ',
+                    path.extname(path.join(pathToFolder,file.name)).substring(1),' - ',
+                    statOfFile.size, ' bytes');  
+      }
+  } catch (err) {
+    console.error(err);
+  }
+}
+readStats('secret-folder');
