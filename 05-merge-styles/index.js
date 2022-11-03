c38a629982dfdccbef01aeb2e055ad401800c769
neemkashu <sparjinkust@mail.ru>
2022-11-03 23:10:56 +0300

    refactor: rename streams and replace callback with promise in task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,8 @@
 import path from 'node:path';
 import { fileURLToPath } from 'url';
-import fs from 'node:fs';
+import { writeFile } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
+import fs from 'node:fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,36 +13,24 @@ async function writeBundle() {
   let pathToFolder = path.join(__dirname, 'styles');
   const files = await readdir(pathToFolder, { withFileTypes: true });
 
-  fs.writeFile(  
-    path.join(__dirname, 'project-dist', 'bundle.css'),
-    '',
-    (err) => {
-      if (err) throw err;
-    }
-  );
+  writeFile(path.join(__dirname, 'project-dist', 'bundle.css'),'')
+  .then( (err) => {
+    if (err) throw err;
+  });   
 
   for (const file of files) {
     const isCSStype = await isCSS(file.name);
-    const input = fs.createReadStream(path.join(__dirname, 'styles', file.name), {encoding: 'utf8'});
-    const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {flags:'a'});
-
-    if (file.isFile() && isCSStype) {
-      
-      
-      input.on('data', chunk => {
-
-        output.write(chunk);
+    const readStream = fs.createReadStream(path.join(__dirname, 'styles', file.name), {encoding: 'utf8'});
+    const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {flags:'a'});
+    
+    if (file.isFile() && isCSStype) {    
+      readStream.on('data', chunk => {
+        writeStream.write(chunk);
       });
-      input.on('error', error => console.log('Error', error.message));
-
+      readStream.on('error', error => console.log('Error', error.message));
     }
-    input.on('exit', () => {
-      console.log('write next style to bundle');
-      output.destroy();
-    });
   }
 }
-
 
 async function isCSS(fileName) {
   try {
