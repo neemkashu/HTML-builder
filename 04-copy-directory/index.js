c57073b8650b3ded087de4f3db8fec3bb06bbc46
neemkashu <sparjinkust@mail.ru>
2022-11-03 16:01:45 +0300

    feat: fix recursive method in task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,38 +3,70 @@ import { mkdir } from 'node:fs/promises';
 import { fileURLToPath } from 'url';
 import { rm } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
-import { stat } from 'node:fs/promises';
-import { copyFile, constants } from 'node:fs/promises';
-
-async function copyDir (folder) {
-  try {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    const newFolder = folder + '-copy';
-    const pathToFolder = path.join( __dirname, folder);
-    const pathNewFolder = path.join(__dirname, newFolder);
-    
-    async function copySubDir(node, ancestorDir) {
-      if (node.isFile() ) { // node is file or directory
-        copyFile(path.join(folder, ancestorDir, node.name), path.join(newFolder, ancestorDir, node.name));
-      }
-      if ( !node.isFile() ) {
-        const children = await readdir(node, { withFileTypes: true });
+//import { access } from 'node:fs/promises';
+import { copyFile } from 'node:fs/promises';
+//import { deflate } from 'node:zlib';
+class Copir {
+  async copyDir(folder) {
+    try {
+      this.__dirname = path.dirname(fileURLToPath(import.meta.url));
+      
+      this.folder = folder;
+      this.newFolder = folder + '-copy';
+      this.pathToFolder = path.join( this.__dirname, this.folder);
+      this.pathNewFolder = path.join(this.__dirname, this.newFolder);
+      
+      let isRemoved = undefined;
+      
+      await rm(this.pathNewFolder, {force: true, recursive: true})
+        .then( async () => {
+          const folderObject = await readdir(this.__dirname, { withFileTypes: true });
+          if (isRemoved === undefined) {
+            mkdir(this.pathNewFolder, { recursive: true }).then( this.copySubDir([undefined], folderObject[0] )); //TODO: fix the [0]
+          }
+        });
+      
+    }
+    catch (err) {
+      console.log('im outsider error');
+      console.error(err);
+    }
+  }
+  // node is file or directory
+  async copySubDir(ancestorDir, node) {
+    if (node.isFile() ) { 
+      copyFile(path.join(this.__dirname, this.folder, ...ancestorDir, node.name), path.join(this.__dirname, this.newFolder, ...ancestorDir, node.name));
+    }
+    if ( !node.isFile() ) {
+      const pathToNode = (ancestorDir[0] === undefined)?
+        path.join(this.__dirname, this.folder):
+        path.join(this.__dirname, this.folder, ...ancestorDir, node.name);
+      
+      if (ancestorDir[0] !== undefined){
+        mkdir( path.join(this.__dirname, this.newFolder, ...ancestorDir, node.name),{ recursive: true } )
+          .then(async () => {
+            const children = await readdir(pathToNode, { withFileTypes: true });
+            for (const child of children) {
+              this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
+            }
+          });
+      } else {
+        const children = await readdir(pathToNode, { withFileTypes: true });
         for (const child of children) {
-          copySubDir(child, path.join(ancestorDir, node.name) );
+          this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
         }
       }
+      
+      
     }
-    const isRemoved = await rm(pathNewFolder, { force: true, maxRetries: 0, recursive: true });
-    const folderObject = await readdir(pathToFolder, { withFileTypes: true });
-    if (isRemoved === undefined) {
-      mkdir(pathNewFolder, { recursive: true })
-      .then( copySubDir(folderObject[0], __dirname ))
-    }
-
-    
   }
-  catch (err) {
-    console.error(err);
+  async _developAncestor(ancestorDir, folder) {
+    switch (ancestorDir[0]) {
+    case undefined: return [''];
+    default: return [...ancestorDir, folder];
+    }
   }
 }
-copyDir('files');
+
+const copir = new Copir;
+copir.copyDir('files');
