af2f12ec144c329b8184b7819b12cce880924c44
neemkashu <sparjinkust@mail.ru>
2022-11-04 21:54:11 +0300

    refactor: delete logs in task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,11 +2,9 @@ import path from 'node:path';
 import { mkdir } from 'node:fs/promises';
 import { fileURLToPath } from 'url';
 import { rm } from 'node:fs/promises';
-//import { copyFile } from 'node:fs/promises';
 import { writeFile } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
 import fs from 'node:fs';
-//import { info } from 'node:console';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -46,8 +44,6 @@ async function buildStyles() {
   try {   
     const pathBundleFolder = path.join(__dirname, 'project-dist');
     
-    //let isRemoved = undefined;
-    
     await rm(pathBundleFolder, {force: true, recursive: true})
       .then( async () => {        
           mkdir(pathBundleFolder, { recursive: true })
@@ -62,8 +58,6 @@ async function buildStyles() {
 async function findComponents() {   
   const pathComponents  = path.join(__dirname, 'components');
   const components = await readdir(pathComponents, { withFileTypes: true });
-  //let componentList = [];
-  //let componentNames = [];
 
   let listComponents = {};
 
@@ -81,15 +75,6 @@ async function findComponents() {
 
 }
 async function cutTemplate (htmlCode){
-
-//   let strExample = 
-// `<body>
-//   {{header}}
-//   <main class="main">
-//     {{articles}}
-//   </main>
-//   {{footer}}
-// </body>`;
 
   let regexpPlaceHolder = /\{\{(.*?)\}\}/ig;
   let regexpSpaces = /[ ]{0,}\{\{/ig;
@@ -135,20 +120,7 @@ async function insertComponents() {
     const placeHolderStr = infoPlaceHolders[component].placeHolder;
     const indentStr = infoPlaceHolders[component].indents;
 
-    console.log('------insert initializing-----');
-    console.log('componentContent', componentContent.slice(0,35), '...');
-    console.log('placeHolderStr', placeHolderStr);
-    console.log('the indent is ---- ', indentStr.length, indentStr.charCodeAt(0));
-
-    console.log(`the char code of original SPACES is ${componentContent.charCodeAt(23)}
-the char code of original ENTERS is ${componentContent.charCodeAt(24)}
-then is vhar code ${componentContent.charCodeAt(25)}`);
-
     let linesOfCode = componentContent.split(String.fromCharCode(13)+String.fromCharCode(10));
-    console.log('LOG one line of code, the same indeces use');
-    console.log(`the char code of line SPACES is ${linesOfCode[0].charCodeAt(22)}
-    the char code of line ENTERS is ${linesOfCode[0].charCodeAt(23)}
-    then is char code ${linesOfCode[0].charCodeAt(24)}`);
     
     let linesWithSpaces = [];
     linesWithSpaces.push(linesOfCode[0]);
@@ -156,11 +128,8 @@ then is vhar code ${componentContent.charCodeAt(25)}`);
       let line = linesOfCode[ i ];
       linesWithSpaces.push(indentStr + line);
     }
-    console.log(linesWithSpaces[0]);
 
     let contentWithIndents = linesWithSpaces.join(`${String.fromCharCode(13)}${String.fromCharCode(10)}`);
-
-    //console.log(contentWithIndents);
 
     promiseTemplateCode = promiseTemplateCode.replace(placeHolderStr,contentWithIndents);
   }
@@ -171,18 +140,7 @@ then is vhar code ${componentContent.charCodeAt(25)}`);
     writeStream.write(promiseTemplateCode);
   }
   );
-
 }
-
 
 buildStyles();
 insertComponents();
-
-// const promiseListComponents = await findComponents();
-// console.log('promiseListComponents', promiseListComponents);
-
-// const promiseTemplate = await readHTML(path.join (__dirname,'template.html'));
-// const infoTemplate = await cutTemplate(promiseTemplate);
-// console.log('info template', infoTemplate);
-// const promiseArticle = await readHTML(path.join (__dirname, 'components', 'articles.html'));
-// console.log('info Article', promiseArticle);
