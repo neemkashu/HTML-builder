b5ea52e7cb068329877c875ce93dfaea6205c976
neemkashu <sparjinkust@mail.ru>
2022-11-04 22:43:50 +0300

    feat: add recursive copying assets in task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,6 +5,7 @@ import { rm } from 'node:fs/promises';
 import { writeFile } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
 import fs from 'node:fs';
+import { copyFile } from 'node:fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -143,5 +144,77 @@ async function insertComponents() {
   );
 }
 
+class Copir {
+  async copyDir(folder, folderCopy) {
+    try {
+      this.__dirname = path.dirname(fileURLToPath(import.meta.url));
+      
+      this.folder = folder;
+      this.newFolder = folderCopy;
+      this.pathToFolder = path.join( this.__dirname, this.folder);
+      this.pathNewFolder = path.join(this.__dirname, 'project-dist', this.newFolder);
+      
+      let isRemoved = undefined;
+      
+      await rm(this.pathNewFolder, {force: true, recursive: true})
+        .then( async () => {
+          const folderObject = await readdir(this.__dirname, { withFileTypes: true });
+          let targetFolder;
+          for (let eachFolder of folderObject) {
+            if (eachFolder.name === folder && !eachFolder.isFile()) {
+              targetFolder = eachFolder;
+              break;
+            } 
+          }
+          if (isRemoved === undefined) {
+            mkdir(this.pathNewFolder, { recursive: true }).then( this.copySubDir([undefined], targetFolder )); 
+          }
+        });
+      
+    }
+    catch (err) {
+      console.log('im outsider error');
+      console.error(err);
+    }
+  }
+  // node is file or directory
+  async copySubDir(ancestorDir, node) {
+    if (node.isFile() ) { 
+      copyFile(path.join(this.__dirname, this.folder, ...ancestorDir, node.name), path.join(this.__dirname,'project-dist', this.newFolder, ...ancestorDir, node.name));
+    }
+    if ( !node.isFile() ) {
+      const pathToNode = (ancestorDir[0] === undefined)?
+        path.join(this.__dirname, this.folder):
+        path.join(this.__dirname, this.folder, ...ancestorDir, node.name);
+      
+      if (ancestorDir[0] !== undefined){
+        mkdir( path.join(this.__dirname,'project-dist', this.newFolder, ...ancestorDir, node.name),{ recursive: true } )
+          .then(async () => {
+            const children = await readdir(pathToNode, { withFileTypes: true });
+            for (const child of children) {
+              this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
+            }
+          });
+      } else {
+        const children = await readdir(pathToNode, { withFileTypes: true });
+        for (const child of children) {
+          this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
+        }
+      }
+      
+      
+    }
+  }
+  async _developAncestor(ancestorDir, folder) {
+    switch (ancestorDir[0]) {
+    case undefined: return [''];
+    default: return [...ancestorDir, folder];
+    }
+  }
+}
+
+const copir = new Copir;
+copir.copyDir('assets','assets');
+
+
 buildStyles();
-//insertComponents();
