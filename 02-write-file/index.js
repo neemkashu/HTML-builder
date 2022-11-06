367bbd0dbc29d54408166418f704947c0ec25b24
neemkashu <sparjinkust@mail.ru>
2022-11-06 20:57:36 +0300

    feat: use readline to save the text in task 02 refactor: wrap the code in async function in task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,36 +1,34 @@
 import path from 'node:path';
 import { fileURLToPath } from 'url';
-import fs from 'node:fs';
+import {writeFile} from 'node:fs/promises';
+import {appendFile} from 'node:fs/promises';
+import * as readline from 'node:readline/promises';
 
-const { stdout, stdin } = process;
-let flagOfSIGINT = 0;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-stdout.write('Type a text and press ENTER\n');
-fs.writeFile(
+async function writeFromTerminal() {
+  const { stdout, stdin } = process;
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const pathToNotes = path.resolve(__dirname, 'notes.txt');
   
-  path.resolve(__dirname, 'notes.txt'),
-  '',
-  (err) => {
-    if (err) throw err;
-  }
-);
-process.on('exit', () => {
-  if (!flagOfSIGINT) stdout.write('The changes are saved in notes.txt');
-});
-process.on('SIGINT', () => {
-  flagOfSIGINT++;
-  stdout.write('The changes are saved in notes.txt');  
-  process.exit();
-});
-stdin.on('data', data => {
-  if (data.toString().slice(0,4) === 'exit') process.exit();
-  fs.appendFile(
-    path.resolve(__dirname, 'notes.txt'),
-    data.toString(),
-    (err) => {
-      if (err) throw err;
-    }
-  );
-});
+  const rl = readline.createInterface({ input: stdin, output:stdout });
 
+  stdout.write('Type a text and press ENTER\n');
+
+  writeFile(pathToNotes,'')
+  .catch( err => console.error(err));
+
+  rl.on('line', (line) => {
+    if (line.toString().slice(0,4) === 'exit') {
+      stdout.write('Exit procedure. The changes are saved in notes.txt');
+      rl.close();
+    } else {
+      appendFile(pathToNotes, line);
+    }
+  });
+
+  rl.once('SIGINT', () => {
+    stdout.write('The changes are saved in notes.txt');
+    rl.close();
+  });
+}
+
+writeFromTerminal();
