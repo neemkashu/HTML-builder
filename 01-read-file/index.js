1926d53475c2472d190bda818d248771bf33fbf2
neemkashu <sparjinkust@mail.ru>
2022-11-06 17:28:12 +0300

    refactor: replace anonymous wrapper with declared function

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,10 +3,11 @@ import { fileURLToPath } from 'url';
 import fs from 'node:fs';
 import { stdout } from 'node:process';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+async function readFile() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"),{encoding: 'utf8'});
 
-(async () => {
-  const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'),{encoding: 'utf8'});
   readableStream.pipe(stdout);
-})();
+}
 
+readFile();
