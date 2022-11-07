6a5808af52a5e4a1b32faefddc2d77d5a3a182ea
neemkashu <sparjinkust@mail.ru>
2022-11-07 13:36:40 +0300

    refactor: add updated Copir from task 04 in task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,6 +6,7 @@ import { writeFile } from 'node:fs/promises';
 import { readdir } from 'node:fs/promises';
 import fs from 'node:fs';
 import { copyFile } from 'node:fs/promises';
+import { stat } from 'node:fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -51,7 +52,7 @@ async function buildStyles() {
           mkdir(pathBundleFolder, { recursive: true })
           .then( bundleStyles )
           .then( insertComponents )
-          .then(copir.copyDir('assets','assets')) 
+          .then(copir.copyDir(path.join('06-build-page','assets'),path.join(pathBundleFolder,'assets')) );
       });
   }
   catch (err) {
@@ -145,72 +146,31 @@ async function insertComponents() {
   }
   );
 }
-
 class Copir {
-  async copyDir(folder, folderCopy) {
-    try {
-      this.__dirname = path.dirname(fileURLToPath(import.meta.url));
-      
-      this.folder = folder;
-      this.newFolder = folderCopy;
-      this.pathToFolder = path.join( this.__dirname, this.folder);
-      this.pathNewFolder = path.join(this.__dirname, 'project-dist', this.newFolder);
-      
-      let isRemoved = undefined;
-      
-      await rm(this.pathNewFolder, {force: true, recursive: true})
-        .then( async () => {
-          const folderObject = await readdir(this.__dirname, { withFileTypes: true });
-          let targetFolder;
-          for (let eachFolder of folderObject) {
-            if (eachFolder.name === folder && !eachFolder.isFile()) {
-              targetFolder = eachFolder;
-              break;
-            } 
-          }
-          if (isRemoved === undefined) {
-            mkdir(this.pathNewFolder, { recursive: true }).then( this.copySubDir([undefined], targetFolder )); 
-          }
-        });
-      
-    }
-    catch (err) {
-      console.log('im outsider error');
-      console.error(err);
-    }
+  
+  async copyDir (oldPath, newPath){
+    rm(newPath, {force: true, recursive: true})
+    .then( async () => {
+      mkdir(newPath, { recursive: true })
+      .then(this.copySubDir(oldPath, newPath))
+      .catch(err => console.error(err));
+    });  
   }
-  // node is file or directory
-  async copySubDir(ancestorDir, node) {
+  
+  async copySubDir(oldPath, newPath) {
+    const node = await stat(oldPath);
+
     if (node.isFile() ) { 
-      copyFile(path.join(this.__dirname, this.folder, ...ancestorDir, node.name), path.join(this.__dirname,'project-dist', this.newFolder, ...ancestorDir, node.name));
+      copyFile(oldPath, newPath);
     }
-    if ( !node.isFile() ) {
-      const pathToNode = (ancestorDir[0] === undefined)?
-        path.join(this.__dirname, this.folder):
-        path.join(this.__dirname, this.folder, ...ancestorDir, node.name);
-      
-      if (ancestorDir[0] !== undefined){
-        mkdir( path.join(this.__dirname,'project-dist', this.newFolder, ...ancestorDir, node.name),{ recursive: true } )
+    if ( node.isDirectory() ) {
+        mkdir( newPath,{ recursive: true } )
           .then(async () => {
-            const children = await readdir(pathToNode, { withFileTypes: true });
+            const children = await readdir(oldPath, { withFileTypes: true });
             for (const child of children) {
-              this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
+              this.copySubDir(path.join(oldPath, child.name), path.join(newPath, child.name));
             }
           });
-      } else {
-        const children = await readdir(pathToNode, { withFileTypes: true });
-        for (const child of children) {
-          this.copySubDir(await this._developAncestor(ancestorDir, node.name), child);
-        }
-      }
-      
-      
-    }
-  }
-  async _developAncestor(ancestorDir, folder) {
-    switch (ancestorDir[0]) {
-    case undefined: return [''];
-    default: return [...ancestorDir, folder];
     }
   }
 }
